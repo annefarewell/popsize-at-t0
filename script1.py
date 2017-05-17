acf084000cd066e52b938c7aacbf941556e5a291
annefarewell <anne.farewell@gu.se>
2017-05-17 11:24:20 +0200

    first change
    first change

---
+++ script1.py
@@ -0,0 +1,14 @@
+#insert template header
+
+#load pandas
+import pandas as pd
+#import data file including gen time, time at GT and pop size at GT
+pd.read_csv('xxx.csv')
+#calculate k
+#calculate pop size at t=0
+#create new file with metadata and pop size at t=0
+#some kind of graph
+
+#if time, import raw data and plot growth curve extrapolating back to t=0, group replicates
+
+#if time, quality assurance, check that time at GT is 'reasonable' :is greater than/less than a user defined value
